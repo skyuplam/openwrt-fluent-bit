055aa9f013e44d84b3b68f7dd3b72b98f9092dae
Terrence Lam <skyuplam@gmail.com>
2018-05-13 10:23:22 +0200

    Fix typo

--- openwrt.cmake
+++ openwrt.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_COMPILER ${tools}/bin/mipsel-openwrt-linux-musl-g++)
 # set(CMAKE_C_FLAGS "-ffreestanding  -fno-stack-protector")
 # set(CMAKE_CXX_FLAGS "-ffreestanding  -fno-stack-protector")
 
-set(CMAKE_FIND_ROOT_PATH ${OPENWRT_TOOLCHAIN_DIR})
+set(CMAKE_FIND_ROOT_PATH $ENV{OPENWRT_TOOLCHAIN_DIR})
 # set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
